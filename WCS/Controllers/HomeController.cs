1d46fe82a028dc1834a15cd4b2b0cf85f9bb613a
Chan <Chan@CH>
2017-03-07 21:50:20 +1100

    Add models and controllers

---
+++ WCS/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WCS.Models;
+
+namespace WCS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var model = new PageModel();
+            var filename = Server.MapPath(@"~/App_Data/SampleData.json");
+
+            model.PopulateDataFromJsonFile(filename);
+            return View(model);
+        }
+    }
+}
---
+++ WCS/Models/PageModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Web.Hosting;
+
+namespace WCS.Models
+{
+    public class PageModel
+    {
+        public List<CarModel> results;
+        public List<CarModel> savedCars;
+
+        public void PopulateDataFromJsonFile(string filename)
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            var tempPageModel = ser.Deserialize<PageModel>(File.ReadAllText(filename));
+            results = tempPageModel.results;
+            savedCars = tempPageModel.savedCars;
+                
+                
+        }
+    }
+}
