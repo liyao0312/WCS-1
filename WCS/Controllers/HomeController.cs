d0e8d8099ff2d66e592c400e5c734ae3ca4303c2
Chan <Chan@CH>
2017-03-08 19:56:04 +1100

    Fix UI. Add hover function.

--- WCS/Controllers/HomeController.cs
+++ WCS/Controllers/HomeController.cs
@@ -11,31 +11,30 @@ namespace WCS.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        Repository repository = new Repository();
         public ActionResult Index()
         {
             var filename = Server.MapPath(@"~/App_Data/SampleData.json");
-            var repository = new Repository();
+            
             var model = repository.PopulatePageModelFromJsonFile(filename);
             return View(model);
         }
+
         public ActionResult Add(string id)
         {
-            var filename = Server.MapPath(@"~/App_Data/SampleData.json");
-            var repository = new Repository();
+            var filename = Server.MapPath(@"~/App_Data/SampleData.json");            
             var model = repository.PopulatePageModelFromJsonFile(filename);
             model = repository.AddToSavedCars(id, model);
             var boolResult = repository.SavePageModelToJsonFile(filename, model);
-
             return RedirectToAction("Index");
         }
+
         public ActionResult Remove(string id)
         {
             var filename = Server.MapPath(@"~/App_Data/SampleData.json");
-            var repository = new Repository();
             var model = repository.PopulatePageModelFromJsonFile(filename);
             model = repository.RemoveFromSavedCars(id, model);
             var boolResult = repository.SavePageModelToJsonFile(filename, model);
-
             return RedirectToAction("Index");
         }
     }
